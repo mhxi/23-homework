36d3940c4ef59107b2bc4af016d0f92e4efbfd8a
mhxi <mhxi91@gmail.com>
2015-12-05 12:02:38 -0800

    moved tic-tac-toe and bog-app

--- mhxi/bog_app/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'creatures#index'
-
-  # make an index route for creatures resource.
-  # get "/creatures", to: "creatures#index"
-  resources :creatures, only: [:index]
-
-end
